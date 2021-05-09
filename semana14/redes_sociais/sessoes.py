24e1a6166969b3f3efd25c11668c90376f65b1d8
Jonatas Silvestrini <silvestrini@nappsolutions.com>
2021-05-09 19:09:26 -0300

    semana14

--- semana14/redes_sociais/sessoes.py
+++ semana14/redes_sociais/sessoes.py
@@ -28,6 +28,7 @@ class PersonalSection(Section):
     def __repr__(self):
         return 'Dados Pessoais'
 
+
 class AlbumSection(Section):
     def sobre(self):
         print("Sessao para fotos")
@@ -37,6 +38,7 @@ class AlbumSection(Section):
 
     def __repr__(self):
         return 'Sessão para fotos'
+
 
 class PublicationSection(Section):
     def describe(self):
@@ -50,3 +52,17 @@ class PublicationSection(Section):
 
     def __repr__(self):
         return 'Sessão publicações'
+
+
+class UploadCodeSection(Section):
+    def sobre(self):
+        print("Sessao para upload de dados")
+
+    def upload(self):
+        print("Sessão para Upload")
+
+    def __str__(self):
+        return 'Sessão Upload'
+
+    def __repr__(self):
+        return 'Sessão Upload'
